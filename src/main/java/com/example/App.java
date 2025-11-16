89b881fd27f8145874dd9c53d6e0f0b3429ec573
Samuel Nartey <narteyotuafosamuel@gmail.com>
2025-11-16 10:07:45 +0000

    change file configurations

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,20 +5,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class App {
 
-    public static void main(String[] args)
-    {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
-    public void init()
-    {
-        Logger log = LoggerFactory.getLogger(App.class);
+    public void init() {
         log.info("Java app started");
     }
 
